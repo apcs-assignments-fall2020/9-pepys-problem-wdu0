02c782d1a6a50b8e70f614cc0065d772230f38c4
wdu0 <23wdu@athenian.org>
2020-10-01 06:34:13 +0000

    Added some code to the methods

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -5,26 +5,82 @@ public class MyMain {
     // Returns in the answer as a double corresponding to the percentage
     // For example, 75.5% would be 75.5
     public static double probabilityOneSix() {
-        // REPLACE WITH YOUR CODE HERE
-        return -1.0;
+        int times = 10000;
+        int total = 0;
+        int numofsix;
+
+        for (int i = 0; i < times; i++) {
+            numofsix = 0;
+            for (int d = 0; d < 6; d++) {
+                double randouble = Math.random() * 6 + 1;
+                int ran = (int) randouble;
+                if (ran == 6) {
+                    numofsix ++;
+                }
+            }
+            if (numofsix >= 1) {
+                total ++;
+            }
+        }
+        double percent = (double) total / (double) times;
+        double result = percent * 100;
+        return result;
     }
 
     // Calculate the probability of rolling at least two 6's when rolling 
     // twelve dice. Uses 10000 trials.
     public static double probabilityTwoSixes() {
-        // REPLACE WITH YOUR CODE HERE
-        return -1.0;
+        int times = 10000;
+        int total = 0;
+        int numofsix;
+
+        for (int i = 0; i < times; i++) {
+            numofsix = 0;
+            for (int d = 0; d < 12; d++) {
+                double randouble = Math.random() * 6 + 1;
+                int ran = (int) randouble;
+                if (ran == 6) {
+                    numofsix ++;
+                }
+            }
+            if (numofsix >= 2) {
+                total ++;
+            }
+        }
+        double percent = (double) total / (double) times;
+        double result = percent * 100;
+        return result;
     }
 
     // Calculate the probability of rolling at least three 6's when rolling 
     // eighteen dice. Uses 10000 trials.
     public static double probabilityThreeSixes() {
-        // REPLACE WITH YOUR CODE HERE
-        return -1.0;
+        int times = 10000;
+        int total = 0;
+        int numofsix;
+
+        for (int i = 0; i < times; i++) {
+            numofsix = 0;
+            for (int d = 0; d < 18; d++) {
+                double randouble = Math.random() * 6 + 1;
+                int ran = (int) randouble;
+                if (ran == 6) {
+                    numofsix ++;
+                }
+            }
+            if (numofsix >= 3) {
+                total ++;
+            }
+        }
+        double percent = (double) total / (double) times;
+        double result = percent * 100;
+        return result;
     }
 
 
     public static void main(String[] args) {
-        // YOUR CODE HERE
+        System.out.println("The % of probability of rolling at least one 6 when rolling 6 dice is " + probabilityOneSix());
+        System.out.println("The % of probability of rolling at least two 6 when rolling 12 dice is " + probabilityTwoSixes());
+        System.out.println("The % of probability of rolling at least three 6 when rolling 18 dice is " + probabilityThreeSixes());
     }
 }
